cd8adad0ca4888f22260614dce169952d7ccf40d
Bogomil <80456128+BonganY23@users.noreply.github.com>
2025-07-01 17:59:52 +0300

    Update prerequisites_k3d.sh

--- prerequisites_k3d.sh
+++ prerequisites_k3d.sh
@@ -10,6 +10,7 @@ cd deploy_kubernetes
 sudo apt-get update
 sudo apt-get -y install ca-certificates curl
 sudo apt update ; sudo apt install --reinstall ca-certificates ; sudo update-ca-certificates
+export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
