dc6c6a741f94e758385ef8f4bb42f96d47273ccf
Bogomil <80456128+BonganY23@users.noreply.github.com>
2025-07-01 17:55:22 +0300

    Update prerequisites_k3d.sh

--- prerequisites_k3d.sh
+++ prerequisites_k3d.sh
@@ -9,6 +9,7 @@ cd deploy_kubernetes
 # Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get -y install ca-certificates curl
+sudo apt update ; sudo apt install --reinstall ca-certificates ; sudo update-ca-certificates
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
